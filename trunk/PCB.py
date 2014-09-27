d93685c927fec75b65e4f6dd9b77c495288abeaa
m.f.melendi@gmail.com <m.f.melendi@gmail.com@1a934f71-88c8-38f8-ed27-e32e2e6d3af8>
2014-09-27 17:05:35 +0000

    PCB

---
+++ trunk/PCB.py
@@ -0,0 +1,9 @@
+class PCB:
+    def __init__(self, ini, fin, pc, pid):
+        self.posicion_ini = ini
+        self.posicion_fin = fin
+        self.pc = pc
+        self.state = New()
+        self.pid = pid
+
+		
