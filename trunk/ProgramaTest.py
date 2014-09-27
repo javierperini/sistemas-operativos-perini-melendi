5b19487aad53c4cb6042d9e515cafb3095bd0d5f
m.f.melendi@gmail.com <m.f.melendi@gmail.com@1a934f71-88c8-38f8-ed27-e32e2e6d3af8>
2014-09-27 17:07:06 +0000

    ProgramaTest

---
+++ trunk/ProgramaTest.py
@@ -0,0 +1,29 @@
+from Programa import *
+from Instruccion import *
+import unittest
+
+
+class ProgramaTest(unittest.TestCase):
+    def setUp(self):
+
+        self.progr = Programa()
+    #Arrange
+        self.progr= Programa()
+        self.instr= Instruccion('Instruccion 1')
+    #Action
+        self.progr.agregarInstruccion(self.instr)
+
+    def test_agregarInstruccionCompararTamanho(self):
+    #Assert
+        self.assertEquals(1, len(self.progr.getInstrucciones()))
+
+    def test_agregarInstruccionCompararElementos(self):
+    #Assert
+        self.assertEquals(self.progr.getElemento(0), self.instr)
+
+    def test_instruccionesOrdenadasEnOutput(self):
+    #Assert
+        self.assertEquals('Instruccion1', self.progr.getInstruccionEjecutada(0))
+
+suite = unittest.TestLoader().loadTestsFromTestCase(ProgramaTest)
+unittest.TextTestRunner(verbosity=2).run(suite)
