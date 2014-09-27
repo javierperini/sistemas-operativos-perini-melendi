bb8a91796a85d270cd5d7f6ab095a468a966007f
m.f.melendi@gmail.com <m.f.melendi@gmail.com@1a934f71-88c8-38f8-ed27-e32e2e6d3af8>
2014-09-27 17:04:58 +0000

    Memoria

---
+++ trunk/Memoria.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class Memoria:
+    def __init__(self):
+        self.registros = np.arrange(512)
+        self.siguientePosicion = 0
+
+    def read(self, posicion):
+        return self.registros[posicion]
+
+    def write(self, posicion, instruccion):
+        self.registros[posicion] = instruccion
+
+    def siquientePosicion(self):
+        return self.siguientePosicion
+
+    def hayEspacioPara(self, tamanhoDelPrograma):
+        return (self.resgistros.size() - self.siguientePosicion()) > tamanhoDelPrograma
+
+
+#prueba
+#m=Memoria()
+#m.write(0,1)
+		
