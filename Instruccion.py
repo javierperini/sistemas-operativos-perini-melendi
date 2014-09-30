a3fa60780191fbd45b190c5d3f3c2994f8733531
javierperini90 <javierperini90@gmail.com>
2014-09-30 22:34:12 +0000

    Testeado: -Disco -Instruccion -Output -Programa
    Modifique: 
    -PCB ( agregue getters y setters)

--- Instruccion.py
+++ Instruccion.py
@@ -1,3 +1,7 @@
+from Output import *
+import unittest
+
+
 class Instruccion:
     def __init__(self, text):
         self.texto = text
@@ -5,3 +9,17 @@ class Instruccion:
     def run(self, output):
         output.agregarInstruccion(self.texto)
 
+
+class TestsOutput(unittest.TestCase):
+
+    def setUp(self):
+        self.output2 = Output()
+        self.instruccion = Instruccion("soy una instruccion")
+
+    def test_correrUnaInstruccion(self):
+        self.instruccion.run(self.output2)
+        self.assertEquals("soy una instruccion", self.output2.get(0))
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestsOutput)
+unittest.TextTestRunner(verbosity=2).run(suite)
--- PCB.py
+++ PCB.py
@@ -1,3 +1,4 @@
+from New import *
 class PCB:
     def __init__(self, ini, fin, pc, pid):
         self.posicion_ini = ini
@@ -6,4 +7,20 @@ class PCB:
         self.state = New()
         self.pid = pid
 
-		
+    def posicionInicial(self):
+        return self.posicion_ini
+
+    def posicionFinal(self):
+        return self.posicion_fin
+
+    def getPc(self):
+        return self.pc
+
+    def getState(self):
+        return self.state
+
+    def getPid(self):
+        return self.pid
+
+    def setState(self, stateNew):
+        self.state= stateNew
