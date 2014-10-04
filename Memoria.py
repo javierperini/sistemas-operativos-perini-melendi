249e4bfdf487986c5e41da1e004d17b921aceaf8
javierperini90 <javierperini90@gmail.com>
2014-10-04 00:41:51 +0000

    Testeado: -Disco -Instruccion -Output -Programa
    Modifique: 
    -PCB ( agregue getters y setters)

--- Memoria.py
+++ Memoria.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 class Memoria:
     def __init__(self):
         self.registros = []
--- PCB.py
+++ PCB.py
@@ -1,10 +1,12 @@
-from New import *
+from Estado import *
+
+
 class PCB:
     def __init__(self, ini, fin, pc, pid):
         self.posicion_ini = ini
         self.posicion_fin = fin
         self.pc = pc
-        self.state = New()
+        self.state = New
         self.pid = pid
 
     def posicionInicial(self):
