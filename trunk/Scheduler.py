eacb1dcc52eee5570587e3eaf1ae1a8b307b0868
camilagarcia.113@gmail.com <camilagarcia.113@gmail.com@1a934f71-88c8-38f8-ed27-e32e2e6d3af8>
2014-10-13 22:24:56 +0000

    pseudo test scheduler

--- trunk/Scheduler.py
+++ trunk/Scheduler.py
@@ -1,4 +1,6 @@
 import Queue
+from PCB import *
+import unittest
 
 
 class Scheduler:
@@ -42,6 +44,44 @@ class PriorityScheduler():
     def add_pcb(self, pcb):
         self._readyQueue.put(pcb)
 
-    class RoundRobinScheduler():
+
+class RoundRobinScheduler():
         def __init__(self, quantum):
-            self.cola = []
+            self.readyQueue = Queue()
+
+
+#--------------------------------------------------------#
+class TestsAdmin(unittest.TestCase):
+    def setUp(self):
+        self.pcb1 = PCB(100, 200, 40, 55, 5)
+        self.pcb2 = PCB(100, 200, 40, 55, 10)
+        self.pcb3 = PCB(100, 200, 40, 55, 2)
+        self.pcb4 = PCB(100, 200, 40, 55, 1)
+        self.pcb5 = PCB(100, 200, 40, 55, 9)
+        self._scheduler = Scheduler()
+
+    def test_fifo_scheduler(self):
+        self.fifo_policy = FifoScheduler()
+        self._scheduler.set_as_fifo()
+        self.fifo_policy.add_pcb(self.pcb1)
+        self.fifo_policy.add_pcb(self.pcb2)
+        self.fifo_policy.add_pcb(self.pcb3)
+
+        self.assertEquals(self._scheduler.get_pcb(), self.pcb1)
+
+    def test_priority_scheduler(self):
+        self.priority_policy = PriorityScheduler()
+        self._scheduler.set_as_priority()
+        self.priority_policy.add_pcb(self.pcb1)
+        self.priority_policy.add_pcb(self.pcb2)
+        self.priority_policy.add_pcb(self.pcb3)
+        self.priority_policy.add_pcb(self.pcb4)
+        self.priority_policy.add_pcb(self.pcb5)
+
+        self.assertEquals(self._scheduler.get_pcb(), 4)
+
+
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestsAdmin)
+unittest.TextTestRunner(verbosity=2).run(suite)
