6e6092943a2b32adcaf8d02488a865a81fdec3b6
m.f.melendi <m.f.melendi@gmail.com>
2014-11-18 00:58:44 +0000

    Muchos cambios

--- Cpu.py
+++ Cpu.py
@@ -1,3 +1,4 @@
+from threading import Thread
 from AlertHandler import Alerter
 
 __author__ = 'memonono'
@@ -9,8 +10,6 @@ class CPU():
         self.alerter = Alerter()
         self.memory = memory
 
-#Threadear aqui |||
-    #           $$$
     def run(self, pcb):
         self.memory.instructions_for(pcb.posicion_ini)
         self.alerter.alert_for(pcb)
@@ -20,6 +19,8 @@ class CPU():
 #Hacer algo
 #Lo que va a hacer es ir a buscar a memoria las instrucciones
 #cargadas del proximo proceso a ejecutar.
+
+#Se crea un nuevo thread cuando el proceso pasa a la cola de ready?
 
 #El reloj tira un Tick cada x cantidad de tiempo
 #puede ser un thread, un while, o usar time y tirar un notify cada x cantidad de tiempo
---
+++ FIle.py
@@ -0,0 +1,9 @@
+from FileSystemComponent import FileSystemComponent
+
+__author__ = 'memonono'
+
+
+class File(FileSystemComponent):
+
+    def __init__(self, size, content, name):
+        super(size, name, content)
