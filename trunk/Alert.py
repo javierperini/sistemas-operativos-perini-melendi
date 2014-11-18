97b939a52a9f59e1c66e436617a89b08ec3c8229
m.f.melendi@gmail.com <m.f.melendi@gmail.com@1a934f71-88c8-38f8-ed27-e32e2e6d3af8>
2014-11-18 23:02:07 +0000

    Alerts arreglado, alerthandler arreglado

--- trunk/Alert.py
+++ trunk/Alert.py
@@ -6,50 +6,61 @@ __author__ = 'memonono'
 
 class InstructionAlert:
 
-        def __init__(self, cpu, scheduler):
-            raise ReferenceError
+    def __init__(self, cpu, scheduler):
+        self.cpu = cpu
+        self.scheduler = scheduler
 
-        def condition_of_applicability(self, pcb):
-            pass
+    def __new__(cls, *args, **kwargs):
+        raise ReferenceError
 
-        def alert_for(self , pcb):
-            new_pcb = self.scheduler.nextPCB()
-            self.cpu.pcb = new_pcb
-            new_pcb.state = ProcessState.ready
-            self.cpu.resetRoundRobin(pcb)
+    def condition_of_applicability(self, pcb):
+        pass
+
+    def alert_cpu(self, pcb):
+        new_pcb = self.scheduler.nextPCB()
+        self.cpu.pcb = new_pcb
+        new_pcb.state = ProcessState.ready
+        self.cpu.resetRoundRobin(pcb)
 
 
 class KillAlert(InstructionAlert):
 
-        def __init__(self, cpu):
-            self.cpu = cpu
+    def __init__(self, cpu, scheduler):
+        super.__init__(cpu, scheduler)
 
-        def condition_of_applicability(self, pcb):
-            return pcb.posicion_fin == pcb.posicion_ini
+    def condition_of_applicability(self, pcb):
+        return pcb.posicion_fin == pcb.posicion_ini
 
-        def alert_for(self, pcb):
-            super
-            pcb.state = ProcessState.end
-            self.cpu.memory.free(pcb)
-            PCBTable.remove(pcb)
+    def alert_cpu(self, pcb):
+        super.alert_cpu(pcb)
+        pcb.state = ProcessState.end
+        self.cpu.memory.free(pcb)
+        PCBTable.remove(pcb)
 
 
 class TimeoutAlert(InstructionAlert):
 
-        def condition_of_applicability(self, pcb):
-            raise NotImplemented
+    def __init__(self, cpu, scheduler):
+        super.__init__(cpu, scheduler)
 
-        def alert_for(self, pcb):
-            super
-            pcb.state = ProcessState.ready
+    def condition_of_applicability(self, pcb):
+        raise NotImplemented
+
+    def alert_cpu(self, pcb):
+        super.alert_cpu(pcb)
+        pcb.state = ProcessState.ready
 
 
 class IOAlert(InstructionAlert):
 
+    def __init__(self, cpu, scheduler):
+        super.__init__(cpu, scheduler)
+
     def condition_of_applicability(self, pcb):
         return pcb.next_instruction().is_io_instruction()
 
-    def alert_for(self, pcb):
+    def alert_cpu(self, pcb):
+        super.alert_cpu(pcb)
         pcb.state = ProcessState.waiting
         #Podriamos hacer algo en el medio, como procesar la IOInstruction
         pcb.state = ProcessState.ready
@@ -57,9 +68,13 @@ class IOAlert(InstructionAlert):
 
 class NewAlert(InstructionAlert):
 
+    def __init__(self, cpu, scheduler):
+        super.__init__(cpu, scheduler)
+
     def condition_of_applicability(self, pcb):
         pcb.state.equals(ProcessState.new)
 
-    def alert_for(self, pcb):
+    def alert_cpu(self, pcb):
+        super.alert_cpu(pcb)
         PCBTable.add(pcb)
         pcb.state = ProcessState.ready
