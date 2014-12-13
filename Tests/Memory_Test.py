c867f8f701cf96ac3eb8099be0cbbb89703340e0
m.f.melendi <m.f.melendi@gmail.com>
2014-12-13 18:37:29 +0000

    Muchos cambios

--- Tests/Memory_Test.py
+++ Tests/Memory_Test.py
@@ -1,27 +1,27 @@
 import unittest
 
-from Memory.Memoria import *
+from Memory.Memory import *
 
 
 class TestsMemoria(unittest.TestCase):
 
     def setUp(self):
-        self.memoria = Memoria()
+        self.memoria = Memory()
         self.memoria.write(0, "primeraInstruccion")
         self.memoria.write(1, "segundaInstruccion")
 
     def test_siguientePosicion(self):
-        self.assertEquals(2, self.memoria.siguientePosicion)
+        self.assertEquals(2, self.memoria.next_position)
 
     def test_hayEspacioParaGuardar(self):
-        self.assertTrue(self.memoria.hayEspacioParaGuardar(10))
+        self.assertTrue(self.memoria.free_memory_to_save(10))
 
     def test_readMemoria(self):
         self.assertEqual("primeraInstruccion", self.memoria.read(0))
 
     def test_deleteMemoria(self):
         self.memoria.delete(1)
-        self.assertEqual(511, self.memoria.getCeldasLibres())
+        self.assertEqual(511, self.memoria.free_cells())
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestsMemoria)
